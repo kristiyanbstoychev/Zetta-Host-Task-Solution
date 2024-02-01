c0ada8a2789f68f4846c9bad423358e80c70c0ab
kristiyanbstoychev <chrisbstoychev@gmail.com>
2024-02-01 15:18:32 +0200

    Added test for verifying the home page content for mobile and desktop devices

--- src/test/java/tests/GlobalVariables.java
+++ src/test/java/tests/GlobalVariables.java
@@ -20,7 +20,7 @@ public class GlobalVariables {
     public static WebDriver driver;
 
     //default browser and device, used in cases where no environment variables are defined
-    public static final String defaultBrowser = "mobile";
+    public static final String defaultBrowser = "chrome";
     public static final String defaultDevice = "iPhone 6/7/8";
     //Samsung Galaxy S20 Ultra
     //Galaxy S9+
